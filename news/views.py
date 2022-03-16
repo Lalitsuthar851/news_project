fa2d06123a06706744710e71a6cdd0ce8c2ab323
unknown <lsuthar8511@gmail.com>
2022-03-16 17:45:15 +0530

    here all the file are staged

---
+++ news/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render,HttpResponse,redirect
+import newspaper
+from newspaper import Article
+# Create your views here.
+
+def home(request,
+         ):
+    return render(request,
+                  "index.html",
+                  )
+
+def scraper(request,
+            ):
+    if request.method == "POST":
+        name = request.POST.get("page")
+        print(name)
+        article = Article(name)
+
+        try:
+            article.download()
+            article.parse()
+            article.nlp()
+
+            keywords = article.keywords
+            print(type(keywords))
+            summmary = article.summary
+            print(summmary,keywords)
+            author = article.authors
+            if author==[]:
+                author=" "
+
+
+            return render(
+                request,
+                "data.html",
+                {"summary":summmary,
+                 "author":author[0],
+                 "keyword":keywords},
+            )
+
+        except:
+            return HttpResponse("please enter valid url")
+
+    return render(request,
+                  "newsscraper.html",
+                  )
+
+def handler404( request,
+               exception
+               ):
+
+    return redirect("home")
+
+def handler500(request,
+               ):
+    return redirect("home")
