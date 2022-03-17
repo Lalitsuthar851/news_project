7e93e35996b49872a6290ff78b09f0db24841d65
unknown <lsuthar8511@gmail.com>
2022-03-17 08:30:59 +0530

    debug true is changed

--- newspaper_features/settings.py
+++ newspaper_features/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4vc@!gr2#_6(1(ua$!2$3yt4h7zerc=&a)&*93!g(4pvs+rs!e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
